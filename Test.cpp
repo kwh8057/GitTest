d323ecb73a15944017e869fa2997061ca58de9a2
Kim WooHyun <kdk7030@gmail.com>
2018-01-04 14:20:54 +0900

    Test First

---
+++ Test.cpp
@@ -0,0 +1,12 @@
+#include "Test.h"
+
+int main() {
+	cout << "Test!!" << endl;
+
+	for (int i=0; i<a; i++) {
+		cout << i;
+	}
+	cout << endl;
+
+	return 0;
+}
---
+++ Test.h
@@ -0,0 +1,11 @@
+#pragma once
+#ifndef __TEST_H__
+#define __TEST_H__
+
+#include <iostream>
+
+using namespace std;
+
+const int a = 20;
+
+#endif
